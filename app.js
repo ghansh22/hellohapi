99ea60601c3191bc1d772447fad3dad9dfff6701
Ghanshyam Baviskar <gnbaviskar@gmail.com>
2018-01-13 10:10:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 'use strict';
 const Hapi = require('hapi');
+const server = new Hapi.Server();
 
-const server = new Hapi.Server({ port: 8088, host: '127.125.1.30' });
-console.log(`Server running at: ${server.info.uri}`);
+// connection
+server.connection({
+    port: 8088,
+    host: 'localhost'
+});
+
+
+// home route
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request,reply)=>{
+        reply('<h3>hello hapi!</h3>')
+    }
+});
+
+// start server
 server.start((err) => {
     if(err) {
         throw err;
     }
-    // console.log(`Server running at: ${server.info.uri}`);
+    console.log(`Server running at: ${server.info.uri}`);
 });
